ba669ef479a13aaab9f65b6d29563a5248ff0718
Marc Wouters <marc.wouters@digipolis.be>
2020-09-28 23:33:34 +0200

    ADD more model attributes;

--- generators/app/templates/src/StarterKit/Api/Models/Examples/Example.cs
+++ generators/app/templates/src/StarterKit/Api/Models/Examples/Example.cs
@@ -2,8 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace StarterKit.Api.Models.Examples
 {
+    /// <summary>
+    /// XML comments added to classes will be visible in Swagger documentation
+    /// These are necessary due to official guidelines
+    /// </summary>
     public class Example : ModelBase
     {
+        /// <summary>
+        /// XML comments added to properties will be visible in Swagger documentation
+        /// These are necessary due to official guidelines
+        /// </summary>
+        /// <example>My name</example>
         [Required(AllowEmptyStrings = false, ErrorMessage = "{0} is required.")]
         public string Name { get; set; }
     }
