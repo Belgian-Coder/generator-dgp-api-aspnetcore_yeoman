0bd4466ebb0f3a81e2947cae30759405c140c227
Marc Wouters <marc.wouters@digipolis.be>
2020-09-28 09:35:16 +0200

    ADD My.ACPaaS platform support;

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var Generator = require('yeoman-generator');
+var OptionOrPrompt = require('yeoman-option-or-prompt');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var del = require('del');
@@ -7,21 +8,36 @@ var nd = require('node-dir');
 var uuidv1 = require('uuid/v1');
 var updateNotifier = require('update-notifier');
 var pkg = require('./../../package.json');
+const fs = require('fs');
 // eslint-disable-next-line no-unused-vars
 var path = require('path');
+const { doesNotMatch, jsonFileContent } = require('assert');
 
 module.exports = class extends Generator {
 
   constructor(args, opts) {
     super(args, opts);
+    // Use optionOrPromt to pass arguments and skipping questions
+    this._optionOrPrompt = OptionOrPrompt;
+    // Save given rootFolder or use folder where yo was executed
+    this.rootFolder = opts['rootFolder'];
+    if (!this.rootFolder) {
+      this.rootFolder = this.contextRoot;
+    }
+    // Use this for My.ACPaaS templates
+    if (opts['myacpaas']) {
+      this.isMyACPaaS = true;
+    };
   }
 
   initializing() {
   }
 
-  prompting() {
-    //var done = this.async();
 
+  paths() {
+  }
+
+  async prompting() {
     // greet the user
     this.log(yosay('Welcome to the fantastic Yeoman ' + chalk.green('dgp-api-aspnetcore') + ' ' + chalk.blue('(' + pkg.version + ')') + ' generator!'));
 
@@ -40,7 +56,7 @@ module.exports = class extends Generator {
       name: 'deleteContent',
       message: 'Delete the contents of this directory before generation (.git will be preserved)? (y/n):',
       default:
-				'y'
+        'y'
     }, {
       type: 'input',
       name: 'projectName',
@@ -62,41 +78,53 @@ module.exports = class extends Generator {
       name: 'dataProvider',
       message: 'Will you be using Entity Framework with MSSQL, PostgreSQL or Not ? (m/p/n):',
       default:
-				'p'
-    }
-    ];
+        'p'
+    }];
 
-    return this.prompt(prompts).then(answers => {
-      this.props = answers; // To access answers later use this.props.someOption;
-      //done();
-    });
+    this.answers = await this._optionOrPrompt(prompts);
+  }
+
+  // Use underscore before function name otherwise it will be invoked by Yeoman
+  _readOptionsFromJson(answers) {
+    var fs = this.fs;
+    var deliverableFile = fs.readJSON(this.rootFolder + "/.deliverable.json");
+    answers.projectName = deliverableFile.deliverable.parameters.projectName;
+    return answers;
   }
 
   writing() {
+    // If My.ACPaaS read options from JSON file(s)
+    if (this.isMyACPaaS) {
+      this.log("Setting answers from MyACPaaS")
+      this.answers = this._readOptionsFromJson(this.answers);
+      this.log(this.answers);
+    }
 
+    this.log("writing");
     // empty target directory
-    console.log('Emptying target directory...');
-    if (this.props.deleteContent === 'y') {
+    if (this.answers.deleteContent === 'y') {
+      this.log('Emptying target directory...');
       del.sync(['**/*', '!.git', '!.git/**/*'], {
         force: true,
         dot: true
       });
     }
 
-    var projectName = this.props.projectName;
+    var projectName = this.answers.projectName;
     var lowerProjectName = projectName.toLowerCase();
 
-    var solutionItemsGuid = uuidv1(); console.log('solutionItemsGuid: ' + solutionItemsGuid + '\r\n');
+    var solutionItemsGuid = uuidv1();
+    this.log('solutionItemsGuid: ' + solutionItemsGuid + '\r\n');
     var srcGuid = uuidv1();
     var testGuid = uuidv1();
     var starterKitGuid = uuidv1();
     var integrationGuid = uuidv1();
     var unitGuid = uuidv1();
 
-    var kestrelHttpPort = this.props.kestrelHttpPort;
-    var iisHttpPort = this.props.iisHttpPort;
-    var iisHttpsPort = this.props.iisHttpsPort;
-    var dataProvider = getDataProvider(this.props.dataProvider, projectName);
+    var kestrelHttpPort = this.answers.kestrelHttpPort;
+    var iisHttpPort = this.answers.iisHttpPort;
+    var iisHttpsPort = this.answers.iisHttpsPort;
+    var dataProvider = getDataProvider(this.answers.dataProvider, projectName);
 
     var copyOptions = {
       process: function (contents) {
@@ -169,17 +197,22 @@ module.exports = class extends Generator {
     };
 
     var source = this.sourceRoot();
-    var dest = this.destinationRoot();
+    var dest = this.rootFolder;
+    this.destinationRoot(this.rootFolder)
     var fs = this.fs;
 
     // copy files and rename starterkit to projectName
-
-    console.log('Creating project skeleton...');
-
+    this.log('Creating project skeleton...');
+    this.log(source + ' => ' + dest);
+    var that = this;
     nd.files(source, function (err, files) {
       for (var i = 0; i < files.length; i++) {
         var ignoreFiles = [];
-        var filename = files[i]
+        var original = files[i];
+        var filename = files[i];
+        var filenameWithoutSource = filename.substring(source.length);
+
+        filenameWithoutSource = filenameWithoutSource
           .replace(/StarterKit/g, projectName)
           .replace(/starterkit/g, lowerProjectName)
           .replace('.npmignore', '.gitignore')
@@ -188,68 +221,69 @@ module.exports = class extends Generator {
           .replace('DataAccessSettings.ms.cs', 'DataAccessSettings.cs')
           .replace('DataAccessSettings.npg.cs', 'DataAccessSettings.cs')
           .replace('DataAccessSettingsConfigKey.ms.cs', 'DataAccessSettingsConfigKey.cs')
-          .replace('DataAccessSettingsConfigKey.npg.cs', 'DataAccessSettingsConfigKey.cs')
-          .replace(source, dest);
+          .replace('DataAccessSettingsConfigKey.npg.cs', 'DataAccessSettingsConfigKey.cs');
+        filename = dest + filenameWithoutSource;
+
+        that.log(original + ' => ' + filename);
         switch (dataProvider.input) {
-        case 'p':
-          if (files[i].indexOf('dataaccess.ms.json') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          if (files[i].indexOf('DataAccessSettings.ms.cs') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          if (files[i].indexOf('DataAccessSettingsConfigKey.ms.cs') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          break;
-        case 'm':
-          if (files[i].indexOf('dataaccess.npg.json') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          if (files[i].indexOf('DataAccessSettings.npg.cs') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          if (files[i].indexOf('DataAccessSettingsConfigKey.npg.cs') > -1) {
-            ignoreFiles.push(files[i]);
-          }
-          break;
-        default:
-          if (
-            files[i].indexOf('EntityContext.cs') > -1 ||
-            files[i].indexOf('DataAccessDefaults.cs') > -1 ||
-            files[i].indexOf('dataaccess.ms.json') > -1 ||
-            files[i].indexOf('dataaccess.npg.json') > -1 ||
-            files[i].indexOf('DataAccessSettings.ms.cs') > -1 ||
-            files[i].indexOf('DataAccessSettings.npg.cs') > -1 ||
-            files[i].indexOf('DataAccessSettingsConfigKey.ms.cs') > -1 ||
-            files[i].indexOf('DataAccessSettingsConfigKey.npg.cs') > -1
-          ) {
-            ignoreFiles.push(files[i]);
-          }
+          case 'p':
+            if (files[i].indexOf('dataaccess.ms.json') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            if (files[i].indexOf('DataAccessSettings.ms.cs') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            if (files[i].indexOf('DataAccessSettingsConfigKey.ms.cs') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            break;
+          case 'm':
+            if (files[i].indexOf('dataaccess.npg.json') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            if (files[i].indexOf('DataAccessSettings.npg.cs') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            if (files[i].indexOf('DataAccessSettingsConfigKey.npg.cs') > -1) {
+              ignoreFiles.push(files[i]);
+            }
+            break;
+          default:
+            if (
+              files[i].indexOf('EntityContext.cs') > -1 ||
+              files[i].indexOf('DataAccessDefaults.cs') > -1 ||
+              files[i].indexOf('dataaccess.ms.json') > -1 ||
+              files[i].indexOf('dataaccess.npg.json') > -1 ||
+              files[i].indexOf('DataAccessSettings.ms.cs') > -1 ||
+              files[i].indexOf('DataAccessSettings.npg.cs') > -1 ||
+              files[i].indexOf('DataAccessSettingsConfigKey.ms.cs') > -1 ||
+              files[i].indexOf('DataAccessSettingsConfigKey.npg.cs') > -1
+            ) {
+              ignoreFiles.push(files[i]);
+            }
         }
 
         if (!ignoreFiles.includes(files[i])) {
           fs.copy(files[i], filename, copyOptions);
         }
+
       }
     });
-
   }
 
   install() {
     // this.installDependencies();
-    //this.log('----');
   }
 };
 
 function getDataProvider(input, projectName) {
   var efCorePackage =
-      '<PackageReference Include="Microsoft.EntityFrameworkCore" Version="3.1.5" />\n';
+    '<PackageReference Include="Microsoft.EntityFrameworkCore" Version="3.1.5" />\n';
   var efDesignPackage =
-      '<PackageReference Include="Microsoft.EntityFrameworkCore.Design" Version="3.1.5">\n' +
-      '<PrivateAssets>all</PrivateAssets>\n' +
-      '<IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>\n' +
-      '</PackageReference>\n';
+    '<PackageReference Include="Microsoft.EntityFrameworkCore.Design" Version="3.1.5">\n' +
+    '<PrivateAssets>all</PrivateAssets>\n' +
+    '<IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>\n' +
+    '</PackageReference>\n';
   var npgSqlPackage =
     '<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="3.1.4" />\n';
   var sqlServerPackage =
@@ -257,10 +291,10 @@ function getDataProvider(input, projectName) {
   var usings = 'using Microsoft.EntityFrameworkCore;\nusing Microsoft.EntityFrameworkCore.Migrations;\nusing Digipolis.DataAccess;\nusing StarterKit.DataAccess;\nusing StarterKit.DataAccess.Options;\nusing Microsoft.EntityFrameworkCore.Diagnostics;'.replace(/StarterKit/g, projectName);
   var programConfig = 'config.AddJsonFile(JsonFilesKey.DataAccessJson);\n';
   var tools =
-      '<PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="3.1.5">\n' +
-      '<PrivateAssets>all</PrivateAssets>\n' +
-      '<IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>\n' +
-      '</PackageReference>\n';
+    '<PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="3.1.5">\n' +
+    '<PrivateAssets>all</PrivateAssets>\n' +
+    '<IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>\n' +
+    '</PackageReference>\n';
   var registerConfiguration =
     'DataAccessSettings.RegisterConfiguration(services, Configuration.GetSection(Shared.Constants.ConfigurationSectionKey.DataAccess), Environment);';
   var variable = 'DataAccessSettings dataAccessSettings;';
@@ -280,14 +314,14 @@ function getDataProvider(input, projectName) {
     getService: ''
   };
 
-  if (input.toLowerCase() === 'p') {
+  if (input.toLowerCase() === 'postgres') {
     dataProvider.package = efCorePackage + efDesignPackage + npgSqlPackage;
     dataProvider.startupServices =
-			'      services.AddDataAccess<EntityContext>()\n' +
-			'      .AddDbContext<EntityContext>(options => {\n' +
-			'      		options.UseNpgsql(dataAccessSettings.GetConnectionString(),\n' +
-			'      		opt => opt.MigrationsHistoryTable(HistoryRepository.DefaultTableName, DataAccessDefaults.SchemaName));\n' +
-			'      });';
+      '      services.AddDataAccess<EntityContext>()\n' +
+      '      .AddDbContext<EntityContext>(options => {\n' +
+      '      		options.UseNpgsql(dataAccessSettings.GetConnectionString(),\n' +
+      '      		opt => opt.MigrationsHistoryTable(HistoryRepository.DefaultTableName, DataAccessDefaults.SchemaName));\n' +
+      '      });';
 
     dataProvider.startupImports = usings;
     dataProvider.programConfig = programConfig;
@@ -295,13 +329,13 @@ function getDataProvider(input, projectName) {
     dataProvider.registerConfiguration = registerConfiguration;
     dataProvider.variable = variable;
     dataProvider.getService = getService;
-  } else if (input.toLowerCase() === 'm') {
+  } else if (input.toLowerCase() === 'mssql') {
     dataProvider.package = efCorePackage + efDesignPackage + sqlServerPackage;
     dataProvider.startupServices =
-		'      services.AddDataAccess<EntityContext>()\n' +
-		'      .AddDbContext<EntityContext>(options => {\n' +
-		'      		options.UseSqlServer(dataAccessSettings.GetConnectionString());\n' +
-		'      });';
+      '      services.AddDataAccess<EntityContext>()\n' +
+      '      .AddDbContext<EntityContext>(options => {\n' +
+      '      		options.UseSqlServer(dataAccessSettings.GetConnectionString());\n' +
+      '      });';
 
     dataProvider.startupImports = usings;
     dataProvider.programConfig = programConfig;
