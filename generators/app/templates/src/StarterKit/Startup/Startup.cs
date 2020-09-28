dcffa15403d558f730c9c94e989662be3583b30a
Marc Wouters <marc.wouters@digipolis.be>
2020-09-28 09:34:32 +0200

    FIX spaces;

--- generators/app/templates/src/StarterKit/Startup/Startup.cs
+++ generators/app/templates/src/StarterKit/Startup/Startup.cs
@@ -30,200 +30,200 @@ using StarterKit.Shared.Swagger;
 
 namespace StarterKit.Startup
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration, IHostEnvironment env)
+    public class Startup
     {
-      ApplicationBasePath = env.ContentRootPath;
-      Configuration = configuration;
-      Environment = env;
-    }
-
-    public IConfiguration Configuration { get; }
-    public string ApplicationBasePath { get; }
-    public IHostEnvironment Environment { get; }
-    private string XmlCommentsPath
-    {
-      get
-      {
-        var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
-        return Path.Combine(ApplicationBasePath, fileName);
-      }
-    }
-
-    public virtual void ConfigureServices(IServiceCollection services)
-    {
-      #region Read settings
-
-      // Check out ExampleController to find out how these configs are injected into other classes
-      AppSettings.RegisterConfiguration(services, Configuration.GetSection(Shared.Constants.ConfigurationSectionKey.AppSettings), Environment);
-      //--dataaccess-registerConfiguration--
-
-      AppSettings appSettings;
-      //--dataaccess-variable--
-
-      using (var provider = services.BuildServiceProvider())
-      {
-        appSettings = provider.GetService<IOptions<AppSettings>>().Value;
-        //--dataaccess-getService--
-      }
-
-      #endregion
-
-      #region Add Correlation and application services
-
-      services.AddCorrelation(options =>
-      {
-        options.CorrelationHeaderRequired = !Environment.IsDevelopment();
-      });
-
-      services.AddApplicationServices(opt =>
-      {
-        opt.ApplicationId = appSettings.ApplicationId;
-        opt.ApplicationName = appSettings.AppName;
-      });
-
-      #endregion
-
-      #region Logging
-
-      services.AddLoggingEngine();
-
-      #endregion
-
-      //--dataaccess-startupServices--
-
-      #region Add routing and versioning
-
-      services
-        .AddRouting(options =>
+        public Startup(IConfiguration configuration, IHostEnvironment env)
         {
-          options.LowercaseUrls = true;
-          options.LowercaseQueryStrings = true;
-        })
-        .AddControllers()
-        .AddNewtonsoftJson(options =>
+            ApplicationBasePath = env.ContentRootPath;
+            Configuration = configuration;
+            Environment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public string ApplicationBasePath { get; }
+        public IHostEnvironment Environment { get; }
+        private string XmlCommentsPath
         {
-          options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-          options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-          options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-          options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-          options.SerializerSettings.MetadataPropertyHandling = MetadataPropertyHandling.Ignore;
-          options.SerializerSettings.Converters.Add(new StringEnumConverter());
-        });
-
-      services
-        .AddApiVersioning(options =>
-        {
-          options.ReportApiVersions = true;
-          options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-          options.AssumeDefaultVersionWhenUnspecified = true;
-          options.UseApiBehavior = false;
-        })
-        .AddVersionedApiExplorer(options =>
-        {
-          options.GroupNameFormat = "'v'VVV";
-          options.SubstituteApiVersionInUrl = true;
-        });
-
-      #endregion
-
-      #region DI and Automapper
-
-      services.AddBusinessServices();
-      services.AddServiceAgentServices();
-      services.AddDataAccessServices();
-
-      services.AddAutoMapper(typeof(Startup).Assembly);
-
-      #endregion
-
-      #region Swagger
-
-      services
-        .AddSwaggerGen(options =>
-        {
-          // Define multiple swagger docs if you have multiple api versions
-          options.SwaggerDoc("v1",
-            new OpenApiInfo
+            get
             {
-              Title = "STARTERKIT API",
-              Version = "v1",
-              Description = "<API DESCRIPTION>",
-              TermsOfService = null,
-              Contact = new OpenApiContact()
-              {
-                Email = "<MAIL>",
-                Name = "<NAME>"
-              }
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(ApplicationBasePath, fileName);
+            }
+        }
+
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            #region Read settings
+
+            // Check out ExampleController to find out how these configs are injected into other classes
+            AppSettings.RegisterConfiguration(services, Configuration.GetSection(Shared.Constants.ConfigurationSectionKey.AppSettings), Environment);
+            //--dataaccess-registerConfiguration--
+
+            AppSettings appSettings;
+            //--dataaccess-variable--
+
+            using (var provider = services.BuildServiceProvider())
+            {
+                appSettings = provider.GetService<IOptions<AppSettings>>().Value;
+                //--dataaccess-getService--
+            }
+
+            #endregion
+
+            #region Add Correlation and application services
+
+            services.AddCorrelation(options =>
+            {
+                options.CorrelationHeaderRequired = !Environment.IsDevelopment();
             });
 
-          options.OperationFilter<AddCorrelationHeaderRequired>();
-          options.OperationFilter<AddAuthorizationHeaderRequired>();
-          options.OperationFilter<RemoveSyncRootParameter>();
-          options.OperationFilter<LowerCaseQueryAndBodyParameterFilter>();
+            services.AddApplicationServices(opt =>
+            {
+                opt.ApplicationId = appSettings.ApplicationId;
+                opt.ApplicationName = appSettings.AppName;
+            });
 
-          if (File.Exists(XmlCommentsPath))
-          {
-            options.IncludeXmlComments(XmlCommentsPath);
-          }
-        });
+            #endregion
 
-      #endregion
+            #region Logging
 
-      #region Global error handling
+            services.AddLoggingEngine();
 
-      services.AddGlobalErrorHandling<ApiExceptionMapper>();
+            #endregion
 
-      #endregion
-    }
+            //--dataaccess-startupServices--
 
-    public void Configure(IApplicationBuilder app,
-                          IApiVersionDescriptionProvider versionProvider,
-                          ILoggerFactory loggerFactory, IHostApplicationLifetime appLifetime)
-    {
-      loggerFactory.AddLoggingEngine(app, appLifetime, Configuration);
+            #region Add routing and versioning
 
-      // Enable Serilog selflogging to console.
-      Serilog.Debugging.SelfLog.Enable(Console.Out);
+            services
+              .AddRouting(options =>
+              {
+                  options.LowercaseUrls = true;
+                  options.LowercaseQueryStrings = true;
+              })
+              .AddControllers()
+              .AddNewtonsoftJson(options =>
+              {
+                  options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                  options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                  options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                  options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                  options.SerializerSettings.MetadataPropertyHandling = MetadataPropertyHandling.Ignore;
+                  options.SerializerSettings.Converters.Add(new StringEnumConverter());
+              });
 
-      app.UseApiExtensions();
+            services
+              .AddApiVersioning(options =>
+              {
+                  options.ReportApiVersions = true;
+                  options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                  options.AssumeDefaultVersionWhenUnspecified = true;
+                  options.UseApiBehavior = false;
+              })
+              .AddVersionedApiExplorer(options =>
+              {
+                  options.GroupNameFormat = "'v'VVV";
+                  options.SubstituteApiVersionInUrl = true;
+              });
 
-      // CORS
-      app.UseCors((policy) =>
-      {
-        policy.AllowAnyHeader();
-        policy.AllowAnyMethod();
-        policy.AllowAnyOrigin();
-      });
+            #endregion
 
-      var rewriteOptions = new RewriteOptions();
-      rewriteOptions.AddRedirect("^$", "swagger");
-      app.UseRewriter(rewriteOptions);
+            #region DI and Automapper
 
-      app.UseAuthentication();
-      app.UseRouting();
+            services.AddBusinessServices();
+            services.AddServiceAgentServices();
+            services.AddDataAccessServices();
 
-      app.UseAuthorization();
-      app.UseEndpoints(endpoints =>
-        endpoints.MapDefaultControllerRoute().RequireAuthorization("IsAuthenticated"));
+            services.AddAutoMapper(typeof(Startup).Assembly);
 
-      app.UseSwagger(options =>
-      {
-        options.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
-        {
-          swaggerDoc.Servers = new List<OpenApiServer>() { new OpenApiServer() { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" } };
-        });
-      });
+            #endregion
 
-      app.UseSwaggerUI(options =>
-      {
-        foreach (var description in versionProvider.ApiVersionDescriptions)
-        {
-          options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-          options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            #region Swagger
+
+            services
+              .AddSwaggerGen(options =>
+              {
+            // Define multiple swagger docs if you have multiple api versions
+            options.SwaggerDoc("v1",
+              new OpenApiInfo
+                  {
+                      Title = "STARTERKIT API",
+                      Version = "v1",
+                      Description = "<API DESCRIPTION>",
+                      TermsOfService = null,
+                      Contact = new OpenApiContact()
+                      {
+                          Email = "<MAIL>",
+                          Name = "<NAME>"
+                      }
+                  });
+
+                  options.OperationFilter<AddCorrelationHeaderRequired>();
+                  options.OperationFilter<AddAuthorizationHeaderRequired>();
+                  options.OperationFilter<RemoveSyncRootParameter>();
+                  options.OperationFilter<LowerCaseQueryAndBodyParameterFilter>();
+
+                  if (File.Exists(XmlCommentsPath))
+                  {
+                      options.IncludeXmlComments(XmlCommentsPath);
+                  }
+              });
+
+            #endregion
+
+            #region Global error handling
+
+            services.AddGlobalErrorHandling<ApiExceptionMapper>();
+
+            #endregion
         }
-      });
+
+        public void Configure(IApplicationBuilder app,
+                              IApiVersionDescriptionProvider versionProvider,
+                              ILoggerFactory loggerFactory, IHostApplicationLifetime appLifetime)
+        {
+            loggerFactory.AddLoggingEngine(app, appLifetime, Configuration);
+
+            // Enable Serilog selflogging to console.
+            Serilog.Debugging.SelfLog.Enable(Console.Out);
+
+            app.UseApiExtensions();
+
+            // CORS
+            app.UseCors((policy) =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+
+            var rewriteOptions = new RewriteOptions();
+            rewriteOptions.AddRedirect("^$", "swagger");
+            app.UseRewriter(rewriteOptions);
+
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+              endpoints.MapDefaultControllerRoute().RequireAuthorization("IsAuthenticated"));
+
+            app.UseSwagger(options =>
+            {
+                options.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+          {
+                  swaggerDoc.Servers = new List<OpenApiServer>() { new OpenApiServer() { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" } };
+              });
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in versionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                }
+            });
+        }
     }
-  }
 }
